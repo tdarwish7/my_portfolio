255e6a8a73cd807df78cae8f0f57299667df2489
Teacher <Teacher>
2016-06-14 20:52:39 -0400

    started my portfolio, hooked up server

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var server= express()
+
+server.get('/', function(request, response){
+  response.sendFile('public/html/index.html', {root: __dirname});
+});
+
+server.get('/about', function(request, response){
+  response.sendFile('public/html/about.html', {root: __dirname});
+});
+
+server.get('/contact', function(request, response){
+  response.sendFile('public/html/contact.html', {root: __dirname});
+});
+
+
+server.listen(8080);
