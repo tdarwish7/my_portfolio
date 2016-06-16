9c5125658bd688ccf235aa81becdcedcda6cf02b
Teacher <Teacher>
2016-06-15 23:52:02 -0400

    added a contact me form and edited index.js to run body-parser

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var express = require('express');
-var server= express();
+var server = express();
+var bodyParser = require('body-parser');
 
 server.use(express.static(__dirname+'/public'));
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({extended: true}));
 
 server.get('/', function(request, response){
   response.sendFile('public/html/index.html', {root: __dirname});
@@ -13,6 +16,10 @@ server.get('/about', function(request, response){
 
 server.get('/contact', function(request, response){
   response.sendFile('public/html/contact.html', {root: __dirname});
+});
+
+server.post('/contact', function(request, response){
+  response.json(request.body);
 });
 
 
